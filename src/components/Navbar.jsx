856bf1897f745b0c7fad87a6f7c91f733463ff45
Asif Ridwan <asifridwan@gmail.com>
2022-05-25 10:05:51 -0700

    Created responsive Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 export default function Navbar() {
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+  function ChangeIcon() {
+    setIsMenuClicked(!isMenuClicked);
+  }
+
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className="logo fade"></div>
+      <div className={isMenuClicked ? "nav-links" : "hidden"}>
+        <span className="fade">About</span>
+        <span className="fade">Projects</span>
+        <span className="fade">Contact</span>
+      </div>
+      <div className="menu" onClick={ChangeIcon}>
+        <i className={isMenuClicked ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+    </nav>
   )
 }
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,6 +1,8 @@
+import Navbar from "../components/Navbar";
+
 function App() {
   return (
-    <div className="App">App qWerty</div>
+    <Navbar />
   );
 }
 
