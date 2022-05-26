03affcee06124fa2db0c946f20a5bda3bc819071
Asif Ridwan <asifridwan@gmail.com>
2022-05-25 21:12:45 -0700

    Created home page section

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <section className="home-page">
+      <div className="home-info-container">
+        <div className="home-info-name">Hello ! I am Asif Ridwan</div>
+        <div className="home-info-id">Full Stack Software Engineer & Music Producer</div>
+        <div className="button-containers">
+          <button className="about-button fade">Read More</button>
+          <button className="projects-button fade">View Projects</button>
+        </div>
+      </div>
+    </section>
+  )
+}
