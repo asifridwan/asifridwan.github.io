fd190e62fc57a2b4ca621dd6d55779858ab8e772
Asif Ridwan <asifridwan@gmail.com>
2022-05-30 18:00:17 -0700

    Fixed a problem on homepage buttons

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <section className="about-page">
+        <div className="box">
+            <div className="circle"></div>
+            <div className="rectangle"></div>
+        </div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+    </section>
+  )
+}
