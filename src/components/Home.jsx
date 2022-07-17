9c83f7946f6622d825115767c4566d0e892963f1
Asif Ridwan <asifridwan@gmail.com>
2022-07-17 20:06:23 +0600

    Fixed the homescreen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,13 @@ export default function Home() {
   return (
     <section className="home-page">
       <div className="home-info-container">
-        <div className="home-info-name">Hello ! I am Asif Ridwan</div>
-        <div className="home-info-id">Full Stack Software Engineer & Music Producer</div>
+        <div className="home-info">
+          <div className="home-icon"></div>
+          <div className="home-tagline">
+            <div className="home-info-name">Hello ! I am Asif Ridwan</div>
+            <div className="home-info-id">A Full Stack Software Engineer who specializes in the MERN Stack</div>
+          </div>
+        </div>
         <div className="button-containers">
           <button className="about-button fade">Read More</button>
           <button className="projects-button fade">View Projects</button>
