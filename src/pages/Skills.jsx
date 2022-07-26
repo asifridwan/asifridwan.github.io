614e701b59ef0786e645a4a80d7cb6231a16147c
Asif Ridwan <asifridwan@gmail.com>
2022-07-26 21:52:57 +0600

    Finished Skills Page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,5 +1,26 @@
 export default function Skills() {
   return (
-    <section className="page">This is the skills page</section>
+    <section className="page">
+      <div className="info-container vertical">
+        <div className="contact-tagline">
+          <p className="home-info-name">Skills</p>
+          <p className="home-info-id">Here's a list of technologies I'm confidently skilled at</p>
+        </div>
+        <div className="skills-wrapper">
+          <div className="skill-box html"></div>
+          <div className="skill-box css"></div>
+          <div className="skill-box js"></div>
+          <div className="skill-box react"></div>
+          <div className="skill-box redux"></div>
+          <div className="skill-box nodejs"></div>
+          <div className="skill-box postgresql"></div>
+          <div className="skill-box mysql"></div>
+          <div className="skill-box mongodb"></div>
+          <div className="skill-box flutter"></div>
+          <div className="skill-box git"></div>
+          <div className="skill-box bash"></div>
+        </div>
+      </div>
+    </section>
   )
 }
