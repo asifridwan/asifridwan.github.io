1ba04474809f6b114ef0d279dd5c191a1597b81f
Asif Ridwan <asifridwan@gmail.com>
2022-07-29 17:20:38 +0600

    Fixed problems with icons

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,8 +5,8 @@ export default function Project({name, desc, keyword, github, main}) {
         <p className="project-description">{desc}</p>
         <p className="project-keywords">{keyword}</p>
         <div className="project-links">
-            <a className="github-link" href={github}><i className="fab fa-github-alt"></i> View GitHub Repo</a>
-            {main && <a className="main-link" href={main}><i className="fas fa-code-branch"></i> View Project</a>}
+            <a className="github-link" href={github}><i className="fa fa-github-alt"></i> View GitHub Repo</a>
+            {main && <a className="main-link" href={main}><i className="fa fa-fa"></i> View Project</a>}
         </div>
     </div>
   )
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,13 +9,13 @@ export default function Contact() {
           <p className="contact-tagline-2">Feel free to hit me up on any of these !</p>
         </div>
         <div className="contact-links">
-          <ClickableIcon link="mailto:asifridwan@gmail.com" icon="far fa-envelope" tooltip="asifridwan@gmail.com" />
-          <ClickableIcon link="tel:+8801920251381" icon="fab fa-whatsapp" tooltip="(+880) 1920251381" />
-          <ClickableIcon link="https://www.linkedin.com/in/asifridwaan/" icon="fab fa-linkedin" tooltip="Asif Ridwan" />
-          <ClickableIcon link="https://github.com/asifridwan" icon="fab fa-github" tooltip="asifridwan" />
-          <ClickableIcon link="https://twitter.com/asifridwaan" icon="fab fa-twitter" tooltip="@asifridwaan" />
-          <ClickableIcon link="https://www.facebook.com/asifridwan2.0/" icon="fab fa-facebook" tooltip="Asif Ridwan" />
-          <ClickableIcon link="https://www.instagram.com/asifridwaan/" icon="fab fa-instagram" tooltip="@asifridwaan" />
+          <ClickableIcon link="mailto:asifridwan@gmail.com" icon="fa fa-envelope" tooltip="asifridwan@gmail.com" />
+          <ClickableIcon link="tel:+8801920251381" icon="fa fa-whatsapp" tooltip="(+880) 1920251381" />
+          <ClickableIcon link="https://www.linkedin.com/in/asifridwaan/" icon="fa fa-linkedin-square" tooltip="Asif Ridwan" />
+          <ClickableIcon link="https://github.com/asifridwan" icon="fa fa-github" tooltip="asifridwan" />
+          <ClickableIcon link="https://twitter.com/asifridwaan" icon="fa fa-twitter" tooltip="@asifridwaan" />
+          <ClickableIcon link="https://www.facebook.com/asifridwan2.0/" icon="fa fa-facebook-official" tooltip="Asif Ridwan" />
+          <ClickableIcon link="https://www.instagram.com/asifridwaan/" icon="fa fa-instagram" tooltip="@asifridwaan" />
         </div>
       </div>
     </section>
